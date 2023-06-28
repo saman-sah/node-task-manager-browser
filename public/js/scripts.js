aa5ec2d15ec0584557ce8039b5c742a66619639f
SamanSah <sahraei.saman91@gmail.com>
2023-06-29 03:15:08 +0330

    complete project

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,11 +4,9 @@ list.addEventListener("click",async (event)=> {
     let target= event.target;
     let liElement= target.parentElement.parentElement
     const id= parseInt(liElement.dataset.id)
-    console.log(id);
     if(target.classList.contains('toggle-btn')) {
-        let response;
         try {
-            response= await axios.post("/toggle-task", { id })
+            const response= await axios.post("/toggle-task", { id })
             if(response.data === true) {
                 location.reload();
             }else {
@@ -16,6 +14,34 @@ list.addEventListener("click",async (event)=> {
             }
         } catch (err) {
             alert(err.response.data)
+        }
+    }else if(target.classList.contains('edit-btn')) {
+        const text= liElement.querySelector("span").innerText;
+        const answer= prompt("Please enter new title: ", text)
+        if(answer) {
+            try {
+                const response= await axios.post("/edit-task", { id,title: answer })
+                if(response.data === true) {
+                    location.reload();
+                }else {
+                    alert(response.data)
+                }
+            } catch (err) {
+                alert(err.response.data)
+            }
+        }
+    }else if(target.classList.contains('delete-btn')) {
+        if(confirm("Are you sure?")) {
+            try {
+                const response= await axios.post("/delete-task", { id })
+                if(response.data === true) {
+                    location.reload();
+                }else {
+                    alert(response.data)
+                }
+            } catch (err) {
+                alert(err.response.data)
+            }
         }
     }
     
